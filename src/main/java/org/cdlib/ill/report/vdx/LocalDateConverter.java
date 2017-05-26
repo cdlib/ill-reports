f5a410cb40356aced774564788d98d751e0cc370
Michael Morris-Pearce <michael.morris-pearce@ucop.edu>
2017-05-25 17:29:41 -0700

    Added a LocalDate converter.

---
+++ src/main/java/org/cdlib/ill/report/vdx/LocalDateConverter.java
@@ -0,0 +1,28 @@
+package org.cdlib.ill.report.vdx;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * TODO: Moves this into common code.
+ * 
+ * @author mmorrisp
+ */
+@Converter(autoApply = true)
+public class LocalDateConverter implements AttributeConverter<LocalDate, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate x) {
+        if (x == null) return null;
+        return Date.valueOf(x);
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date y) {
+        if (y == null) return null;
+        return y.toLocalDate();
+    }
+    
+}
