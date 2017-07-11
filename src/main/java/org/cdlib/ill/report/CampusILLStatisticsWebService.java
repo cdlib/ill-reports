38709519d8c3fdab69119a09245f10af1678d0d8
Michael Morris-Pearce <michael.morris-pearce@ucop.edu>
2017-07-11 10:50:50 -0700

    Removed XML, JSON for borrowing/lending summaries because it is not required.

--- src/main/java/org/cdlib/ill/report/CampusILLStatisticsWebService.java
+++ src/main/java/org/cdlib/ill/report/CampusILLStatisticsWebService.java
@@ -7,7 +7,6 @@ import java.io.Writer;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.cdlib.ill.model.CampusILLReport;
 import org.cdlib.ill.report.vdx.procedures.SpVdxBorrowingByCategory;
 import org.cdlib.ill.report.vdx.procedures.SpVdxBorrowingOCLCRepository;
 import org.cdlib.ill.report.vdx.procedures.SpVdxBorrowingUCRepository;
@@ -16,9 +15,6 @@ import org.cdlib.ill.report.vdx.procedures.SpVdxLending;
 import org.cdlib.ill.report.vdx.procedures.SpVdxLendingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,29 +30,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CampusILLStatisticsWebService {
 
     @Autowired
-    private CampusILLReportService reportService;
-    @Autowired
     private SpVdxBorrowingUCRepository spVdxBorrowingUCRepo;
     @Autowired
     private SpVdxBorrowingOCLCRepository spVdxBorrowingOCLCRepo;
     @Autowired
     private SpVdxLendingRepository spVdxLendingRepo;
-
-    @RequestMapping(value = "{campusCode}.xml", produces = {"application/xml"})
-    public HttpEntity<CampusILLReport> getCampusXml(
-            @PathVariable("campusCode") String campusCode,
-            @RequestParam(required = false, name = "startDate", defaultValue = "1900-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
-            @RequestParam(required = false, name = "endDate", defaultValue = "2100-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate) {
-        return new ResponseEntity<>(reportService.getILLCampusReport(campusCode, startDate, endDate), HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "{campusCode}.json", produces = {"application/json"})
-    public HttpEntity<CampusILLReport> getCampusJson(
-            @PathVariable("campusCode") String campusCode,
-            @RequestParam(required = false, name = "startDate", defaultValue = "1900-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
-            @RequestParam(required = false, name = "endDate", defaultValue = "2100-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate) {
-        return new ResponseEntity<>(reportService.getILLCampusReport(campusCode, startDate, endDate), HttpStatus.OK);
-    }
 
     @RequestMapping(value = "{campusCode}/borrowing_uc.csv", produces = {"text/csv"})
     public void getVdxBorrowingUCCsv(Writer output,
