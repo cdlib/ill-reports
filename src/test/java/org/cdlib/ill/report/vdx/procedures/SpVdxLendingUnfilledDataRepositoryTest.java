dd89b423cd0e716c7252d80706e4d8263f667f55
Michael Morris-Pearce <michael.morris-pearce@ucop.edu>
2017-07-21 15:59:56 -0700

    Tests for Lending Unfilled.

--- src/test/java/org/cdlib/ill/report/vdx/procedures/SpVdxLendingUnfilledDataRepositoryTest.java
+++ src/test/java/org/cdlib/ill/report/vdx/procedures/SpVdxLendingUnfilledDataRepositoryTest.java
@@ -25,18 +25,18 @@ public class SpVdxLendingUnfilledDataRepositoryTest {
     @Test
     public void testGetLendingUnfilledDetail() {
         stubNativeQueryResultList(em, Arrays.asList(
-                new Object[]{"UCI", "Library A", "", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", "Loan"},
-                new Object[]{"UCI", "Library B", "", "Library C", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", "Loan"},
-                new Object[]{"UCI", "Library C", "", "Library D", "3", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", "Loan"},
-                new Object[]{"UCI", "Library D", "", "Library E", "4", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", "Loan"},
-                new Object[]{"UCI", "Library E", "", "Library A", "5", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", "Loan"}
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library C", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library C", "UC Library", "Library D", "3", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library D", "UC Library", "Library E", "4", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library E", "UC Library", "Library A", "5", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
         ));
         Assert.assertEquals(Sets.newSet(
-                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library A", "", "Library B", 1L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", VdxServiceType.Loan),
-                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library B", "", "Library C", 2L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", VdxServiceType.Loan),
-                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library C", "", "Library D", 3L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", VdxServiceType.Loan),
-                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library D", "", "Library E", 4L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", VdxServiceType.Loan),
-                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library E", "", "Library A", 5L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", VdxServiceType.Loan)
+                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library A", "UC Library", "Library B", 1L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", VdxServiceType.Loan),
+                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library B", "UC Library", "Library C", 2L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", VdxServiceType.Loan),
+                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library C", "UC Library", "Library D", 3L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", VdxServiceType.Loan),
+                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library D", "UC Library", "Library E", 4L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", VdxServiceType.Loan),
+                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library E", "UC Library", "Library A", 5L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", VdxServiceType.Loan)
         ), repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet())
         );
     }
@@ -44,10 +44,157 @@ public class SpVdxLendingUnfilledDataRepositoryTest {
     @Test(expected = IllegalArgumentException.class)
     public void testGetLendingUnfilledDetailWhenProcedureGivesWrongOutputs() {
         stubNativeQueryResultList(em, Arrays.asList(
-                new Object[]{"UCI", "Library A", "", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", "Loan", "extra"},
-                new Object[]{"UCI", "Library B", "", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "serial", "Loan"}
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan", "extra"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenCampusIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{null, "Library B", "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+
+    @Test
+    public void testGetLendingUnfilledDetailWhenCampusIsBlank() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"", "Library B", "UC Library", "Library C", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        Assert.assertEquals(Sets.newSet(
+                new SpVdxLendingUnfilledDetail(VdxCampus.Irvine, "Library A", "UC Library", "Library B", 1L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", VdxServiceType.Loan),
+                new SpVdxLendingUnfilledDetail(VdxCampus.None, "Library B", "UC Library", "Library C", 2L, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", VdxServiceType.Loan)
+        ), repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet())
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenCampusIsNew() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCZ", "Library B", "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenBorrowingLibraryNameIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", null, "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenBorrowingLibraryTypeIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", null, "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenLendingLibraryNameIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", null, "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenIllnoIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", null, "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenTitleIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", null, "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenPublicationDateIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", "The Title", null, "MIT Press", "on the shelf", "laziness", "Serial", "Loan"}
         ));
         repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
     }
     
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenPublisherIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", "The Title", "1999", null, "on the shelf", "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenShelfIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", null, "laziness", "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenAnswerIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", null, "Serial", "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenMaterialTypeIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", null, "Loan"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenServiceTypeIsNull() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", null}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenServiceTypeIsBlank() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", ""}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetLendingUnfilledDetailWhenServiceTypeIsNew() {
+        stubNativeQueryResultList(em, Arrays.asList(
+                new Object[]{"UCI", "Library A", "UC Library", "Library B", "1", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Loan"},
+                new Object[]{"UCI", "Library B", "UC Library", "Library A", "2", "The Title", "1999", "MIT Press", "on the shelf", "laziness", "Serial", "Telepathy"}
+        ));
+        repo.getLendingUnfilledDetail(null, null, null).collect(Collectors.toSet());
+    }
 }
