ec1bf069826a6be2ce4b32682d1bfb3f1e5ba892
Michael Morris-Pearce <michael.morris-pearce@ucop.edu>
2017-08-11 13:27:11 -0700

    Added lending tat xlsx.

--- src/main/java/org/cdlib/ill/report/excel/XLSXController.java
+++ src/main/java/org/cdlib/ill/report/excel/XLSXController.java
@@ -1,15 +1,15 @@
-package org.cdlib.ill.report.api;
+package org.cdlib.ill.report.excel;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.time.LocalDate;
 import java.util.stream.Collectors;
-import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DataConsolidateFunction;
-import org.cdlib.ill.report.excel.ReportWorkbookBuilder;
 import org.cdlib.ill.report.vdx.VdxCampus;
 import org.cdlib.ill.report.vdx.procedures.SpVdxBorrowingTat;
 import org.cdlib.ill.report.vdx.procedures.SpVdxBorrowingTatRepository;
+import org.cdlib.ill.report.vdx.procedures.SpVdxLendingTat;
+import org.cdlib.ill.report.vdx.procedures.SpVdxLendingTatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -23,11 +23,13 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author mmorrisp
  */
 @Controller
-@RequestMapping("/ill/stats/by-campus/")
+@RequestMapping("/ill/reports/by-campus/")
 public class XLSXController {
 
     @Autowired
     private SpVdxBorrowingTatRepository spVdxBorrowingTatRepo;
+    @Autowired
+    private SpVdxLendingTatRepository spVdxLendingTatRepo;
 
     @RequestMapping(
             value = "{campusCode}/borrowing_tat.xlsx",
@@ -45,7 +47,7 @@ public class XLSXController {
                 .fieldText("delivery method", tat -> tat.getShipDeliveryMethod().getCode())
                 .fieldNum("total", tat -> tat.getCount())
                 .data(spVdxBorrowingTatRepo.getBorrowingTat(
-                        VdxCampus.fromCode(campusCode).map(VdxCampus::getCode).orElse(""),
+                        VdxCampus.fromCode(campusCode).map(VdxCampus::getCode).orElse("%"),
                         startDate,
                         endDate).collect(Collectors.toList()))
                 .pivotRow(0)
@@ -58,4 +60,32 @@ public class XLSXController {
                 .write(output);
     }
 
+    @RequestMapping(
+            value = "{campusCode}/lending_tat.xlsx",
+            produces = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    public void getLendingTat(
+            @PathVariable("campusCode") String campusCode,
+            OutputStream output,
+            @RequestParam(required = false, name = "startDate", defaultValue = "1900-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+            @RequestParam(required = false, name = "endDate", defaultValue = "2100-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate) throws IOException {
+        ReportWorkbookBuilder.newWorkbook(SpVdxLendingTat.class)
+                .fieldText("lending campus", tat -> tat.getRespCampus().getCode())
+                .fieldText("lending library", tat -> tat.getRespName())
+                .fieldNum("days", tat -> tat.getTat())
+                .fieldText("service type", tat -> tat.getServiceTp().getCode())
+                .fieldText("delivery method", tat -> tat.getShipDeliveryMethod().getCode())
+                .fieldNum("total", tat -> tat.getCount())
+                .data(spVdxLendingTatRepo.getLendingTat(
+                        VdxCampus.fromCode(campusCode).map(VdxCampus::getCode).orElse("%"),
+                        startDate,
+                        endDate).collect(Collectors.toList()))
+                .pivotRow(0)
+                .pivotRow(1)
+                .pivotRow(2)
+                .pivotColumn(3)
+                .pivotColumn(4)
+                .pivotValue(5, DataConsolidateFunction.SUM, "# of Responses per # of Days")
+                .build()
+                .write(output);
+    }
 }
