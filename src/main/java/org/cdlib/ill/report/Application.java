40433642a428c87646331dea1cb06c9c045ea2ee
Michael Morris-Pearce <michael.morris-pearce@ucop.edu>
2017-09-18 13:23:50 -0700

    Enabled mandatory SSL.

--- src/main/java/org/cdlib/ill/report/Application.java
+++ src/main/java/org/cdlib/ill/report/Application.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.PropertySources;
 @ComponentScan("org.cdlib.ill.report")
 @PropertySources({
     @PropertySource("file:jreport-jdbc.properties")
+    , @PropertySource("file:ssl.properties")
     , @PropertySource("classpath:application.properties")})
 @SpringBootApplication
 public class Application {
