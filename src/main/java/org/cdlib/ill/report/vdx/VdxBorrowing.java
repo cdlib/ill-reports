a8d4b817d0d8fd957ae590827c9c4fd1e0f77e4c
lliu <lliu@192.168.0.12>
2023-02-10 14:24:21 -0800

    Revert VdxBorrowing

--- src/main/java/org/cdlib/ill/report/vdx/VdxBorrowing.java
+++ src/main/java/org/cdlib/ill/report/vdx/VdxBorrowing.java
@@ -1,28 +1,11 @@
 package org.cdlib.ill.report.vdx;
 
 import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
-@SuppressWarnings("serial")
 @Entity
 @Table(name = "vdx_borrowing")
 public class VdxBorrowing extends VdxData implements Serializable {
-  
-  @Column(name = "entry_date", nullable = false)
-  @JsonProperty("entry_date")
-  private Date entryDateDate;
 
-  public Date getEntryDateDate() {
-    return entryDateDate;
-  }
-
-  public void setEntryDateDate(Date entryDate) {
-    this.entryDateDate = entryDate;
-  }
-
-  
 }
