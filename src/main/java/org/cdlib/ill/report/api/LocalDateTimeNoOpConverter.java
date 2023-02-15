c25323f3278ce3bba1ef66902370a21174c8cbb4
jferrie <joe.ferrie@ucop.edu>
2023-02-15 09:05:26 -0800

    more doc

--- src/main/java/org/cdlib/ill/report/api/LocalDateTimeNoOpConverter.java
+++ src/main/java/org/cdlib/ill/report/api/LocalDateTimeNoOpConverter.java
@@ -1,11 +1,18 @@
 package org.cdlib.ill.report.api;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import org.springframework.lang.Nullable;
 
+/*
+ * This converter is used to prevent the framework from running its default converter on
+ * DateTime instances provided by the MySQL 8 connector. The value expected by the ORM framework is java.Util.Date,
+ * which causes a ClassCastException.
+ * 
+ * We couldn't find any way to simply turn off the default behavior, so this NoOp converter is applied
+ * to all DateTime entity fields.
+ */
 @Converter
 public class LocalDateTimeNoOpConverter implements AttributeConverter<LocalDateTime, LocalDateTime> {
 
