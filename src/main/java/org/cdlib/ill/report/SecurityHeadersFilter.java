6762293afba1964b4ecf3a6d9ec0c8b101fa9c51
Joseph Ferrie <joe.ferrie@ucop.edu>
2025-07-29 10:15:52 -0700

    adding security header filter

---
+++ src/main/java/org/cdlib/ill/report/SecurityHeadersFilter.java
@@ -0,0 +1,41 @@
+package org.cdlib.ill.report;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@WebFilter("/*")
+@Order(2)
+public class SecurityHeadersFilter implements Filter {
+
+  private static Logger logger = LoggerFactory.getLogger(SecurityHeadersFilter.class);
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+            throws IOException, ServletException {
+        
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        httpResponse.setHeader("X-Content-Type-Options", "nosniff");
+        httpResponse.setHeader("X-Frame-Options", "DENY");
+        httpResponse.setHeader("X-XSS-Protection", "1; mode=block");
+        httpResponse.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+        httpResponse.setHeader("Content-Security-Policy", "default-src 'self'");
+
+        logger.debug("Set security headers.");
+        
+        chain.doFilter(request, response);
+    }
+}
